f7cdda0c798a29816ba4843e33ad5fc1607a56c8
radishsoups <theobsidianheart@gmail.com>
2025-04-18 13:22:13 -0400

    Updating DB connection

--- web-app/app.py
+++ web-app/app.py
@@ -1,18 +1,23 @@
+import os
 from flask import Flask, render_template
 from pymongo import MongoClient
-import os
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 
+# Load environment variables from the .env file
+load_dotenv()
+
 # connect to mongodb
-mongo_uri = os.getenv("MONGO_URI", "mongodb://localhost:27017")
-client = MongoClient(mongo_uri)
+client = MongoClient(os.getenv("MONGO_URI"))
 db = client["mydb"]  # default database
+users = db["users"]
+
 
 @app.route("/")
 def home():
     return render_template("landing.html")
 
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
-å
