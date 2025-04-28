2581138efaec8ec13532cb06a9f62770e40a8c4b
ChrisimKim <ck3388@nyu.edu>
2025-04-27 22:59:55 -0400

    responsive designs

--- web-app/app.py
+++ web-app/app.py
@@ -698,5 +698,5 @@ def upload_image():
     return jsonify({"status": "error"})
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 5001))
     app.run(host="0.0.0.0", port=port, debug=True)
