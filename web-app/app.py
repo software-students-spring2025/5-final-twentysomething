02509bbfc7f993da034063887249617653912272
ChrisimKim <ck3388@nyu.edu>
2025-04-28 16:10:51 -0400

    altered design for dashboard, header, spin and questionnaire

--- web-app/app.py
+++ web-app/app.py
@@ -427,40 +427,45 @@ def save_and_redirect(recipe_id):
         return redirect(url_for("login"))
 
     user = users.find_one({"username": session["username"]})
-    if user:
-        try:
-            if int(recipe_id) <= 17840:
-                response = requests.get(
-                    f"https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i={recipe_id}"
-                )
-                response.raise_for_status()
-                data = response.json()
-                cocktail = data['drinks'][0]
+    if not user:
+        return redirect(url_for("login"))
 
-                users.update_one({"_id": user["_id"]}, {
-                    "$addToSet": {
-                        "saved_drinks": {
-                            "id": str(cocktail['idDrink']),
-                            "strDrink": cocktail['strDrink'],
-                            "strDrinkThumb": cocktail['strDrinkThumb']
-                        }
-                    }
-                })
-            else:
-                cocktail = additional_drinks.find_one({"idDrink": recipe_id})
+    cocktail = None
 
-                users.update_one({"_id": user["_id"]}, {
-                    "$addToSet": {
-                        "saved_drinks": {
-                            "id": str(cocktail['idDrink']),
-                            "strDrink": cocktail['strDrink'],
-                            "strDrinkThumb": cocktail['strDrinkThumb']
-                        }
-                    }
-                })
+    try:
+        # CASE 1: CocktailDB API
+        if int(recipe_id) <= 17840:
+            response = requests.get(
+                f"https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i={recipe_id}"
+            )
+            response.raise_for_status()
+            data = response.json()
 
-        except requests.exceptions.RequestException as e:
-            print("Error fetching cocktail data:", e)
+            if data.get("drinks"):
+                cocktail = data["drinks"][0]
+
+        # CASE 2: MongoDB drink
+        else:
+            cocktail = additional_drinks.find_one({"idDrink": recipe_id})
+
+        # final check
+        if not cocktail:
+            print(f"No cocktail found for recipe_id {recipe_id}")
+            return redirect(url_for('saved'))
+
+        users.update_one({"_id": user["_id"]}, {
+            "$addToSet": {
+                "saved_drinks": {
+                    "id": str(cocktail.get('idDrink', recipe_id)),
+                    "strDrink": cocktail.get('strDrink', 'Unknown Drink'),
+                    "strDrinkThumb": cocktail.get('strDrinkThumb', ''),
+                    "strInstructions": cocktail.get('strInstructions', ''),
+                }
+            }
+        })
+
+    except Exception as e:
+        print(f"Error saving drink: {e}")
 
     return redirect(url_for('saved'))
 
